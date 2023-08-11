448507102e2e8c714c69e029f3c89b86e065a081
ccbccbccb <bsr068821@gmail.com>
2023-08-11 10:14:31 +0900

    인터페이스

---
+++ chapter03/src/ch12/HomeAppliances.java
@@ -0,0 +1,10 @@
+package ch12;
+
+public abstract class HomeAppliances {
+	
+	int width;
+	int height;
+	String color;
+	
+
+}
---
+++ chapter03/src/ch12/SoundEffect.java
@@ -0,0 +1,9 @@
+package ch12;
+
+public interface SoundEffect {
+	
+	// 인터페이스 안에 추상 메서드는
+	// 가능한 작게 설계 하는것이 좋다.
+	public abstract void notification();
+
+}
